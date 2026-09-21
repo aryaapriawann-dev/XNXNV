f7881aed5f98f9ebd74a187e2ce99138066ad256
aryaapriawann-dev <aryaapriawann@gmail.com>
2026-09-21 11:28:38 +0800

    docs: tambah komentar tipe pada fungsi

--- src/components/TestimonialCard.tsx
+++ src/components/TestimonialCard.tsx
@@ -1,5 +1,6 @@
 // docs: tambah komentar tipe pada fungsi
 // docs: tambah komentar tipe pada fungsi
+// docs: tambah komentar tipe pada fungsi
 /**
  * TestimonialCard component for rendering individual testimonial with avatar, rating, and author info.
  */
