3441778308e940361e19bcb8b50d986dd6bea885
aryaapriawann-dev <aryaapriawann@gmail.com>
2026-09-21 11:28:26 +0800

    docs: tambah komentar tipe pada fungsi

--- src/hooks/useOnScreen.ts
+++ src/hooks/useOnScreen.ts
@@ -1,5 +1,6 @@
 // docs: tambah komentar tipe pada fungsi
 // docs: tambah komentar tipe pada fungsi
+// docs: tambah komentar tipe pada fungsi
 "use client";
 
 import { RefObject, useEffect, useState } from "react";
