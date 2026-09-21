dce646652a615916caf36ea471a34f359e2b04fd
aryaapriawann-dev <aryaapriawann@gmail.com>
2026-09-21 11:28:22 +0800

    chore: rapikan import urutan

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -1,5 +1,6 @@
 // chore: rapikan import urutan
 // chore: rapikan import urutan
+// chore: rapikan import urutan
 import { useState, useEffect } from 'react';
 import Theme, { getThemeFromStorage, setThemeToStorage, isDarkTheme } from './theme';
 
