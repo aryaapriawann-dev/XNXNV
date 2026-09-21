935ad867425ac6bbdac45b9206d3f4d968c095d8
aryaapriawann-dev <aryaapriawann@gmail.com>
2026-09-21 11:28:17 +0800

    docs: tambah komentar tipe pada fungsi

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -1,5 +1,6 @@
 // docs: tambah komentar tipe pada fungsi
 // docs: tambah komentar tipe pada fungsi
+// docs: tambah komentar tipe pada fungsi
 /**
  * Format date to locale string (e.g., "12 September 2026")
  *
