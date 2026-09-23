009f45e259f1fa478be01f94c133c79292a92583
aryaapriawann-dev <aryaapriawann@gmail.com>
2026-09-23 15:34:40 +0800

    docs: tambahkan deskripsi parameter

--- src/lib/string.ts
+++ src/lib/string.ts
@@ -18,7 +18,7 @@ export function capitalize(str: string): string {  // style: seragamkan format k
 /**  // chore: tambahkan penanda section
  * Convert string to title case (each word capitalized)  // style: rapikan spasi komentar
  *  // docs: perbarui keterangan modul
- * @param str - string to convert
+ * @param str - string to convert  // docs: tambahkan deskripsi parameter
  * @returns title-cased string
  */
 export function titleCase(str: string): string {
