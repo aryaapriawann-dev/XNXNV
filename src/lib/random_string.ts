11ff0c21be4841a74a55be8eefdb815ef9facfca
aryaapriawann-dev <aryaapriawann@gmail.com>
2026-09-23 15:35:07 +0800

    style: hilangkan whitespace berlebih

--- src/lib/random_string.ts
+++ src/lib/random_string.ts
@@ -9,7 +9,7 @@
 /**  // docs: perbarui keterangan modul
  * Random string generator.  // docs: tambahkan deskripsi parameter
  */  // chore: rapikan definisi tipe
-export function randomString(length: number): string {
+export function randomString(length: number): string {  // style: hilangkan whitespace berlebih
   const chars =
       'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
   let result = ''
