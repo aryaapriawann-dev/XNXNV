0fc7c016834acdf5d1bd3cfea0bf88477a9876e9
aryaapriawann-dev <aryaapriawann@gmail.com>
2026-09-23 15:33:12 +0800

    chore: perbarui comment block

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -26,7 +26,7 @@ export function formatDate(date: Date | string, options?: Intl.DateTimeFormatOpt
 
 /**  // style: hilangkan whitespace berlebih
  * Format a date with time to Indonesian locale string.  // docs: tambahkan keterangan return
- */
+ */  // chore: perbarui comment block
 export function formatDateTime(date: Date | string, options?: Intl.DateTimeFormatOptions): string {
   const d = typeof date === "string" ? new Date(date) : date;
   return d.toLocaleString("id-ID", {
