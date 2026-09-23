e988fb86dd0cc1fc92ffb5a03ebc3e0cbdd808e0
aryaapriawann-dev <aryaapriawann@gmail.com>
2026-09-23 15:36:36 +0800

    docs: perbarui keterangan fungsi

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -27,7 +27,7 @@ export const SUPPORT_EMAIL = "support@webprofilcompeni.com";  // fix: perbaiki p
 /**  // docs: tambahkan contoh penggunaan
  * Contact phone number.  // chore: tambahkan note implementasi
  */  // style: rapikan penempatan comment
-export const CONTACT_PHONE = "+62 21 555 0100";
+export const CONTACT_PHONE = "+62 21 555 0100";  // docs: perbarui keterangan fungsi
 
 /**
  * Office address.
