2884ea1c5c74569f91c12bb7d8e5bfb1e770e228
aryaapriawann-dev <aryaapriawann@gmail.com>
2026-09-23 15:30:05 +0800

    docs: tambahkan penjelasan variabel

--- src/components/Skeleton.tsx
+++ src/components/Skeleton.tsx
@@ -3,6 +3,7 @@
 // docs: tambah komentar tipe pada fungsi
 // docs: tambah komentar tipe pada fungsi
 // docs: tambah komentar tipe pada fungsi
+// docs: tambahkan penjelasan variabel
 "use client";
 /**
  * Skeleton component for loading placeholders.
