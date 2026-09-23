4cf3ff506c22f64789ffa7c4f9b4a30e38a6a6ee
aryaapriawann-dev <aryaapriawann@gmail.com>
2026-09-23 15:28:42 +0800

    docs: jelaskan tujuan helper

--- src/hooks/useInterval.ts
+++ src/hooks/useInterval.ts
@@ -2,6 +2,7 @@
 // docs: jelaskan tujuan helper
 // docs: jelaskan tujuan helper
 // docs: jelaskan tujuan helper
+// docs: jelaskan tujuan helper
 "use client";
 
 import { useEffect, useRef } from "react";
