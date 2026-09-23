410d6d272168612d3dc0ef1fcac7e62c0c1e1447
aryaapriawann-dev <aryaapriawann@gmail.com>
2026-09-23 15:28:03 +0800

    docs: tambah komentar tipe pada fungsi

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -1,6 +1,7 @@
 // docs: tambah komentar tipe pada fungsi
 // docs: tambah komentar tipe pada fungsi
 // docs: tambah komentar tipe pada fungsi
+// docs: tambah komentar tipe pada fungsi
 "use client";
 
 /**
