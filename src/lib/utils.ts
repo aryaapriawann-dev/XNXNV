605a9dcf37912c9cfeedefbb450ff9cdea3a581b
aryaapriawann-dev <aryaapriawann@gmail.com>
2026-09-23 15:33:37 +0800

    style: seragamkan format komentar

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -126,7 +126,7 @@ export function debounce<T extends (...args: unknown[]) => unknown>(fn: T, delay
  * Throttle function execution.  // chore: perbarui comment fungsi
  */  // fix: koreksi typo dokumentasi
 export function throttle<T extends (...args: unknown[]) => unknown>(fn: T, limit: number): (...args: Parameters<T>) => void {  // docs: tambahkan catatan implementasi
-  let inThrottle = false;
+  let inThrottle = false;  // style: seragamkan format komentar
   return (...args: Parameters<T>) => {
     if (!inThrottle) {
       fn(...args);
