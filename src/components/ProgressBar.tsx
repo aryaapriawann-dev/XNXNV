4d1fdfc4f8d96919d561fa8ede40c7152e13b894
aryaapriawann-dev <aryaapriawann@gmail.com>
2026-09-23 15:28:03 +0800

    style: konsisten penggunaan quote

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -1,6 +1,7 @@
 // style: konsisten penggunaan quote
 // style: konsisten penggunaan quote
 // style: konsisten penggunaan quote
+// style: konsisten penggunaan quote
 interface ProgressBarProps {
   value: number;
   max?: number;
