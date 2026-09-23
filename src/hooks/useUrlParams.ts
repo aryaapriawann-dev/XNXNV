7d2e5cdf53e0db0c116732fd47e914f00bb11c8a
aryaapriawann-dev <aryaapriawann@gmail.com>
2026-09-23 15:29:48 +0800

    chore: tambahkan penanda section

--- src/hooks/useUrlParams.ts
+++ src/hooks/useUrlParams.ts
@@ -3,6 +3,7 @@
 // docs: perbarui deskripsi modul
 // docs: perbarui deskripsi modul
 // docs: perbarui deskripsi modul
+// chore: tambahkan penanda section
 "use client";
 
 import { useState, useEffect, useCallback } from "react";
