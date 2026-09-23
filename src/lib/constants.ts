16ff7dbe623643246c7e56cb6cf8929f5136b319
aryaapriawann-dev <aryaapriawann@gmail.com>
2026-09-23 15:36:47 +0800

    docs: perbarui keterangan modul

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -88,7 +88,7 @@ export const LANGUAGE_KEY = "language";  // style: seragamkan format komentar
  */  // chore: tambahkan penanda section
 export const CONSENT_KEY = "consent";  // style: rapikan spasi komentar
 
-/**
+/**  // docs: perbarui keterangan modul
  * Available items per page options for pagination.
  */
 export const PER_PAGE_OPTIONS = [6, 12, 24, 48];
