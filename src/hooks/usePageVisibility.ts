1db72a3bb4f0417b135f99a7ff92b8d67aa36ab7
aryaapriawann-dev <aryaapriawann@gmail.com>
2026-09-23 15:29:51 +0800

    chore: hilangkan spasi berlebih

--- src/hooks/usePageVisibility.ts
+++ src/hooks/usePageVisibility.ts
@@ -3,6 +3,7 @@
 // chore: hilangkan spasi berlebih
 // chore: hilangkan spasi berlebih
 // chore: hilangkan spasi berlebih
+// chore: hilangkan spasi berlebih
 'use client';
 
 import { useState, useEffect } from 'react';
