8eeca15c92f3fe1f6d9ed68832a7ada3cbb96dd4
aryaapriawann-dev <aryaapriawann@gmail.com>
2026-09-23 15:33:54 +0800

    docs: tambahkan referensi fungsi

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -194,7 +194,7 @@ export function cn(...classes: (string | boolean | undefined | null)[]): string 
  * Generate random string of specified length.  // docs: tambahkan catatan implementasi
  */  // style: seragamkan format komentar
 export function randomString(length: number): string {  // chore: pisahkan blok komentar
-  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";  // docs: tambahkan referensi fungsi
   let result = "";
   for (let i = 0; i < length; i++) {
     result += chars.charAt(Math.floor(Math.random() * chars.length));
