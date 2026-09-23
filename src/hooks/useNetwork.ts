cfeaace0da6aa709a04d2f336239642dffe52674
aryaapriawann-dev <aryaapriawann@gmail.com>
2026-09-23 15:28:41 +0800

    chore: rapikan import urutan

--- src/hooks/useNetwork.ts
+++ src/hooks/useNetwork.ts
@@ -2,6 +2,7 @@
 // chore: rapikan import urutan
 // chore: rapikan import urutan
 // chore: rapikan import urutan
+// chore: rapikan import urutan
 import { useState, useEffect } from 'react';
 
 type NetworkStatus = 'online' | 'offline';
