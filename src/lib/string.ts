b248ec070b04265f443d3e8f84a94dd82c927fed
aryaapriawann-dev <aryaapriawann@gmail.com>
2026-09-23 15:27:44 +0800

    chore: tambah separator komentar bagian

--- src/lib/string.ts
+++ src/lib/string.ts
@@ -2,6 +2,7 @@
 // chore: tambah separator komentar bagian
 // chore: tambah separator komentar bagian
 // chore: tambah separator komentar bagian
+// chore: tambah separator komentar bagian
 /**
  * Capitalize first letter of a string
  *
