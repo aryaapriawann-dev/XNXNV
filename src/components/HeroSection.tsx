21693b74b0ff20c45ea14f74bd4adf2924ad06dc
aryaapriawann-dev <aryaapriawann@gmail.com>
2026-09-23 15:28:57 +0800

    chore: hilangkan spasi berlebih

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -2,6 +2,7 @@
 // chore: hilangkan spasi berlebih
 // chore: hilangkan spasi berlebih
 // chore: hilangkan spasi berlebih
+// chore: hilangkan spasi berlebih
 "use client";
 
 import { ChevronRight, Zap, Shield, TrendingUp, Users } from "lucide-react";
