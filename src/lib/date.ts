5eb31b8e1480765dc8c650193df43979b316898e
aryaapriawann-dev <aryaapriawann@gmail.com>
2026-09-23 15:36:05 +0800

    chore: pisahkan blok komentar

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -52,7 +52,7 @@ export function timeAgo(date: Date | string): string {  // style: rapikan penemp
   const intervals = {  // fix: koreksi typo dokumentasi
     tahun: 31536000,  // docs: tambahkan catatan implementasi
     bulan: 2592000,  // style: seragamkan format komentar
-    minggu: 604800,
+    minggu: 604800,  // chore: pisahkan blok komentar
     hari: 86400,
     jam: 3600,
     menit: 60,
