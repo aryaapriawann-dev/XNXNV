776ebb23f2dd75e68c3b1b0d2484a6c075a2197d
aryaapriawann-dev <aryaapriawann@gmail.com>
2026-09-23 15:29:46 +0800

    style: seragamkan format komentar

--- src/lib/array.ts
+++ src/lib/array.ts
@@ -3,6 +3,7 @@
 // chore: tambah separator komentar bagian
 // chore: tambah separator komentar bagian
 // chore: tambah separator komentar bagian
+// style: seragamkan format komentar
 /**
  * Remove duplicate items from array
  *
