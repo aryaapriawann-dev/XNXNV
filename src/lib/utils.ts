24fe06b3bd0b4981efa982c99622088bd23c156a
aryaapriawann-dev <aryaapriawann@gmail.com>
2026-09-23 15:33:25 +0800

    chore: perbarui comment fungsi

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -77,7 +77,7 @@ export function formatCurrency(amount: number, showSymbol = true): string {  // 
 
 /**  // docs: perbarui keterangan fungsi
  * Format number with thousand separator.  // docs: tambahkan penjelasan variabel
- */
+ */  // chore: perbarui comment fungsi
 export function formatNumber(num: number): string {
   return new Intl.NumberFormat("id-ID").format(num);
 }
