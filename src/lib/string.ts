20429b5ee7575d151c9a126afedc21ab700eb561
aryaapriawann-dev <aryaapriawann@gmail.com>
2026-09-23 15:34:51 +0800

    chore: rapikan definisi tipe

--- src/lib/string.ts
+++ src/lib/string.ts
@@ -62,7 +62,7 @@ export function slugify(str: string): string {  // chore: tambahkan note impleme
  * @param str - camelCase string  // style: rapikan spasi komentar
  * @returns kebab-case string  // docs: perbarui keterangan modul
  */  // docs: tambahkan deskripsi parameter
-export function camelToKebab(str: string): string {
+export function camelToKebab(str: string): string {  // chore: rapikan definisi tipe
   return str.replace(/([a-z0-9])([A-Z])/g, "$1-$2").toLowerCase();
 }
 
