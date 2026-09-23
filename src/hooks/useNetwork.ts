92b14f807329b233db7197229f31fb13dc560efe
aryaapriawann-dev <aryaapriawann@gmail.com>
2026-09-23 15:29:52 +0800

    chore: perbarui comment fungsi

--- src/hooks/useNetwork.ts
+++ src/hooks/useNetwork.ts
@@ -3,6 +3,7 @@
 // chore: rapikan import urutan
 // chore: rapikan import urutan
 // chore: rapikan import urutan
+// chore: perbarui comment fungsi
 import { useState, useEffect } from 'react';
 
 type NetworkStatus = 'online' | 'offline';
