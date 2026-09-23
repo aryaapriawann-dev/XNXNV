a6f77f7eface7514a651a9aec76de09ed3cbea83
aryaapriawann-dev <aryaapriawann@gmail.com>
2026-09-23 15:36:36 +0800

    chore: perbarui comment fungsi

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -30,7 +30,7 @@ export const SUPPORT_EMAIL = "support@webprofilcompeni.com";  // fix: perbaiki p
 export const CONTACT_PHONE = "+62 21 555 0100";  // docs: perbarui keterangan fungsi
 
 /**  // docs: tambahkan penjelasan variabel
- * Office address.
+ * Office address.  // chore: perbarui comment fungsi
  */
 export const OFFICE_ADDRESS = "Jakarta Selatan, Indonesia";
 
