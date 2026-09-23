d4df7488cd055be9fab88e0e38a0778d39c5750c
aryaapriawann-dev <aryaapriawann@gmail.com>
2026-09-23 15:27:57 +0800

    chore: hilangkan spasi berlebih

--- src/components/TestimonialSection.tsx
+++ src/components/TestimonialSection.tsx
@@ -1,6 +1,7 @@
 // chore: hilangkan spasi berlebih
 // chore: hilangkan spasi berlebih
 // chore: hilangkan spasi berlebih
+// chore: hilangkan spasi berlebih
 /**
  * TestimonialSection component displaying testimonials with stats summary.
  * Includes rating display and client satisfaction metrics.
