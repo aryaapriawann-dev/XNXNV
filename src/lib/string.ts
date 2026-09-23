78b514e3e65bf4c859701501ab77e8650605ee17
aryaapriawann-dev <aryaapriawann@gmail.com>
2026-09-23 15:34:36 +0800

    docs: tambahkan penjelasan variabel

--- src/lib/string.ts
+++ src/lib/string.ts
@@ -7,7 +7,7 @@
 // chore: tambah separator komentar bagian
 /**  // style: rapikan penempatan comment
  * Capitalize first letter of a string  // docs: perbarui keterangan fungsi
- *
+ *  // docs: tambahkan penjelasan variabel
  * @param str - string to capitalize
  * @returns string with first letter in uppercase
  */
