073923a289626ca70fffa5f1b817b593ae61270e
aryaapriawann-dev <aryaapriawann@gmail.com>
2026-09-23 15:28:40 +0800

    chore: tambah separator komentar bagian

--- src/hooks/useScroll.ts
+++ src/hooks/useScroll.ts
@@ -2,6 +2,7 @@
 // chore: tambah separator komentar bagian
 // chore: tambah separator komentar bagian
 // chore: tambah separator komentar bagian
+// chore: tambah separator komentar bagian
 'use client';
 
 import { useState, useEffect, useCallback } from 'react';
