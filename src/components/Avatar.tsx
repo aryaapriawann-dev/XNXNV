e83fca4f236162d424355f12d6a1c7e3de69ae62
aryaapriawann-dev <aryaapriawann@gmail.com>
2026-09-23 15:27:55 +0800

    chore: hilangkan spasi berlebih

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,6 +1,7 @@
 // chore: hilangkan spasi berlebih
 // chore: hilangkan spasi berlebih
 // chore: hilangkan spasi berlebih
+// chore: hilangkan spasi berlebih
 import { User } from "lucide-react";
 
 /**
