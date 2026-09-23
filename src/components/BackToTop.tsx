4bcf83ad5a091cc20ff9709b683aaffba9af25bf
aryaapriawann-dev <aryaapriawann@gmail.com>
2026-09-23 15:28:47 +0800

    docs: tambah komentar tipe pada fungsi

--- src/components/BackToTop.tsx
+++ src/components/BackToTop.tsx
@@ -2,6 +2,7 @@
 // docs: tambah komentar tipe pada fungsi
 // docs: tambah komentar tipe pada fungsi
 // docs: tambah komentar tipe pada fungsi
+// docs: tambah komentar tipe pada fungsi
 "use client";
 
 import { useEffect } from "react";
