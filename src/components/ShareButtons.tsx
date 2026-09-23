9d97573a1ae8a06b393a0d7887ace19538fc120a
aryaapriawann-dev <aryaapriawann@gmail.com>
2026-09-23 15:30:05 +0800

    chore: perbarui comment fungsi

--- src/components/ShareButtons.tsx
+++ src/components/ShareButtons.tsx
@@ -3,6 +3,7 @@
 // chore: rapikan import urutan
 // chore: rapikan import urutan
 // chore: rapikan import urutan
+// chore: perbarui comment fungsi
 "use client";
 /**
  * Share buttons component for social media sharing.
