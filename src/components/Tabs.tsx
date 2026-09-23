73fc2c28b296af05b7dec66ce989aba829b59a6d
aryaapriawann-dev <aryaapriawann@gmail.com>
2026-09-23 15:30:02 +0800

    chore: tambah baris dokumentasi

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -3,6 +3,7 @@
 // chore: tambah baris dokumentasi
 // chore: tambah baris dokumentasi
 // chore: tambah baris dokumentasi
+// chore: tambah baris dokumentasi
 "use client";
 
 import { ReactNode, useState } from "react";
