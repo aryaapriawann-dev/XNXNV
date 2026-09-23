58807861a01b116acc5d7eff8ea8bd83c0cdc80b
aryaapriawann-dev <aryaapriawann@gmail.com>
2026-09-23 15:37:21 +0800

    docs: perbarui keterangan modul

--- src/lib/array.ts
+++ src/lib/array.ts
@@ -48,7 +48,7 @@ export function chunk<T>(arr: T[], size: number): T[][] {  // chore: perbarui co
 }  // chore: tambahkan penanda section
 
 /**  // style: rapikan spasi komentar
- * Shuffle array randomly (Fisher-Yates algorithm)
+ * Shuffle array randomly (Fisher-Yates algorithm)  // docs: perbarui keterangan modul
  *
  * @param arr - array to shuffle
  * @returns new shuffled array
