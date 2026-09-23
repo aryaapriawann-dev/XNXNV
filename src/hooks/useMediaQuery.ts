42f5644a365da7365ebaa4f93820c97ba3870ff2
aryaapriawann-dev <aryaapriawann@gmail.com>
2026-09-23 15:29:53 +0800

    fix: koreksi typo dokumentasi

--- src/hooks/useMediaQuery.ts
+++ src/hooks/useMediaQuery.ts
@@ -3,6 +3,7 @@
 // chore: tambah baris dokumentasi
 // chore: tambah baris dokumentasi
 // chore: tambah baris dokumentasi
+// fix: koreksi typo dokumentasi
 import { useEffect, useState } from "react";
 
 interface UseMediaQueryState {
