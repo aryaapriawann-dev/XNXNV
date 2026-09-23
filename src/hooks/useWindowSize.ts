02e615202153d9a4e7390668be7bea220c724866
aryaapriawann-dev <aryaapriawann@gmail.com>
2026-09-23 15:28:38 +0800

    chore: tambah judul section komentar

--- src/hooks/useWindowSize.ts
+++ src/hooks/useWindowSize.ts
@@ -2,6 +2,7 @@
 // chore: tambah judul section komentar
 // chore: tambah judul section komentar
 // chore: tambah judul section komentar
+// chore: tambah judul section komentar
 'use client';
 
 import { useState, useEffect, useCallback } from 'react';
