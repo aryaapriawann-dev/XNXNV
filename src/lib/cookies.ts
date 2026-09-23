986788ad15e89a91f3d8ff1e610d052010f15fc8
aryaapriawann-dev <aryaapriawann@gmail.com>
2026-09-23 15:36:12 +0800

    docs: tambahkan keterangan return

--- src/lib/cookies.ts
+++ src/lib/cookies.ts
@@ -4,7 +4,7 @@
 // chore: rapikan import urutan
 // chore: rapikan import urutan
 // chore: perbarui comment fungsi
-import { cookies } from "next/headers";
+import { cookies } from "next/headers";  // docs: tambahkan keterangan return
 import { THEME_KEY, CONSENT_KEY } from "./constants";
 
 /**
