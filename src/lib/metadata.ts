7d8147358406a0dac16ee2c0fcba061fa22bd5f4
aryaapriawann-dev <aryaapriawann@gmail.com>
2026-09-23 15:27:45 +0800

    chore: hilangkan spasi berlebih

--- src/lib/metadata.ts
+++ src/lib/metadata.ts
@@ -1,6 +1,7 @@
 // chore: hilangkan spasi berlebih
 // chore: hilangkan spasi berlebih
 // chore: hilangkan spasi berlebih
+// chore: hilangkan spasi berlebih
 import { Metadata } from "next";
 
 /**
