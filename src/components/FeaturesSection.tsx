c2f7f1309f13231ffc680053b9a40970b938b5cd
aryaapriawann-dev <aryaapriawann@gmail.com>
2026-09-23 15:28:58 +0800

    chore: tambah baris dokumentasi

--- src/components/FeaturesSection.tsx
+++ src/components/FeaturesSection.tsx
@@ -2,6 +2,7 @@
 // chore: tambah baris dokumentasi
 // chore: tambah baris dokumentasi
 // chore: tambah baris dokumentasi
+// chore: tambah baris dokumentasi
 "use client";
 
 import { Zap, Shield, TrendingUp, Users, Mail, Phone, Clock, Globe, Cpu, Layout } from "lucide-react";
