c5c08a22dcd10da529d7939d5bbf9c921296d676
aryaapriawann-dev <aryaapriawann@gmail.com>
2026-09-23 15:28:06 +0800

    chore: rapikan import urutan

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,7 @@
 // chore: rapikan import urutan
 // chore: rapikan import urutan
 // chore: rapikan import urutan
+// chore: rapikan import urutan
 import Link from "next/link";
 // chore: tambah separator komentar bagian
 
