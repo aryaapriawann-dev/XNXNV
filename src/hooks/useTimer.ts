3fe494d25a7f9c744ff51971da095c09795b9309
aryaapriawann-dev <aryaapriawann@gmail.com>
2026-09-23 15:27:48 +0800

    style: konsisten penggunaan quote

--- src/hooks/useTimer.ts
+++ src/hooks/useTimer.ts
@@ -1,6 +1,7 @@
 // style: konsisten penggunaan quote
 // style: konsisten penggunaan quote
 // style: konsisten penggunaan quote
+// style: konsisten penggunaan quote
 import { useState, useEffect, useCallback } from "react";
 
 interface UseTimerOptions {
