24e82722330483abec0d6442ad5d904cf0397d98
aryaapriawann-dev <aryaapriawann@gmail.com>
2026-09-23 15:27:54 +0800

    chore: tambah judul section komentar

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -1,6 +1,7 @@
 // chore: tambah judul section komentar
 // chore: tambah judul section komentar
 // chore: tambah judul section komentar
+// chore: tambah judul section komentar
 "use client";
 
 import { ReactNode, useState } from "react";
