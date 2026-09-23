114ea433597619fa65e0c98befce92aca0fef076
aryaapriawann-dev <aryaapriawann@gmail.com>
2026-09-23 15:27:52 +0800

    style: konsisten penggunaan quote

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,6 +1,7 @@
 // style: konsisten penggunaan quote
 // style: konsisten penggunaan quote
 // style: konsisten penggunaan quote
+// style: konsisten penggunaan quote
 "use client";
 
 import { useEffect, useState } from "react";
