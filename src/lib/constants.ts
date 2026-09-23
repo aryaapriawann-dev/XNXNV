d293419ff1847e4bc77a4b29279513df4c0ecea3
aryaapriawann-dev <aryaapriawann@gmail.com>
2026-09-23 15:36:31 +0800

    chore: tambahkan penanda section

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -12,7 +12,7 @@ export const APP_NAME = "WEB PROFIL COMPENI";  // docs: tambahkan catatan implem
 /**  // style: seragamkan format komentar
  * Application version constant.  // chore: pisahkan blok komentar
  */  // docs: tambahkan referensi fungsi
-export const APP_VERSION = "1.0.0";
+export const APP_VERSION = "1.0.0";  // chore: tambahkan penanda section
 
 /**
  * Application description constant.
