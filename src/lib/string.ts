ce08c94fe07a77ee1463782219d16c69cfbd6eb7
aryaapriawann-dev <aryaapriawann@gmail.com>
2026-09-23 15:34:53 +0800

    docs: tambahkan penjelasan variabel

--- src/lib/string.ts
+++ src/lib/string.ts
@@ -72,6 +72,6 @@ export function camelToKebab(str: string): string {  // chore: rapikan definisi 
  * @param str - kebab-case string  // chore: tambahkan note implementasi
  * @returns camelCase string  // style: rapikan penempatan comment
  */  // docs: perbarui keterangan fungsi
-export function kebabToCamel(str: string): string {
+export function kebabToCamel(str: string): string {  // docs: tambahkan penjelasan variabel
   return str.replace(/-([a-z])/g, (_, letter) => letter.toUpperCase());
 }
