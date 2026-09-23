a08ad365ebca2e355b1bd854fe5bd1165b3d644f
aryaapriawann-dev <aryaapriawann@gmail.com>
2026-09-23 15:28:48 +0800

    chore: tambah judul section komentar

--- src/components/Textarea.tsx
+++ src/components/Textarea.tsx
@@ -2,6 +2,7 @@
 // chore: tambah judul section komentar
 // chore: tambah judul section komentar
 // chore: tambah judul section komentar
+// chore: tambah judul section komentar
 import { TextareaHTMLAttributes, forwardRef } from "react";
 
 /**
