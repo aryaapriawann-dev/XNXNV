0fe1693d59c780d1d2d3cf4815e682b8a34a96c0
aryaapriawann-dev <aryaapriawann@gmail.com>
2026-09-23 15:27:51 +0800

    chore: tambah baris dokumentasi

--- src/hooks/useMediaQuery.ts
+++ src/hooks/useMediaQuery.ts
@@ -1,6 +1,7 @@
 // chore: tambah baris dokumentasi
 // chore: tambah baris dokumentasi
 // chore: tambah baris dokumentasi
+// chore: tambah baris dokumentasi
 import { useEffect, useState } from "react";
 
 interface UseMediaQueryState {
