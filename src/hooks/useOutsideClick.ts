863b0eaa6716f89b1556b1a233a8ef6b12ee5eff
aryaapriawann-dev <aryaapriawann@gmail.com>
2026-09-23 15:27:50 +0800

    style: konsisten penggunaan quote

--- src/hooks/useOutsideClick.ts
+++ src/hooks/useOutsideClick.ts
@@ -1,6 +1,7 @@
 // style: konsisten penggunaan quote
 // style: konsisten penggunaan quote
 // style: konsisten penggunaan quote
+// style: konsisten penggunaan quote
 import { useEffect, useCallback, useState } from "react";
 
 /**
