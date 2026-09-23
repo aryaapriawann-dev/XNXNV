8b51f80d0508e0ce684434ef97faa5a832421f88
aryaapriawann-dev <aryaapriawann@gmail.com>
2026-09-23 15:28:44 +0800

    style: rapikan indentasi docstring

--- src/hooks/useAccessibility.ts
+++ src/hooks/useAccessibility.ts
@@ -2,6 +2,7 @@
 // style: rapikan indentasi docstring
 // style: rapikan indentasi docstring
 // style: rapikan indentasi docstring
+// style: rapikan indentasi docstring
 import { useState, useEffect } from "react";
 
 /**
