4757830892eff0f8838db548c1c115afa0a71e16
aryaapriawann-dev <aryaapriawann@gmail.com>
2026-09-23 15:30:07 +0800

    style: rapikan spasi komentar

--- src/components/Radio.tsx
+++ src/components/Radio.tsx
@@ -3,6 +3,7 @@
 // chore: hilangkan spasi berlebih
 // chore: hilangkan spasi berlebih
 // chore: hilangkan spasi berlebih
+// style: rapikan spasi komentar
 import { InputHTMLAttributes, forwardRef } from "react";
 
 /**
