8f9e8782fe123b0274847c748e4b554c0e1995ed
aryaapriawann-dev <aryaapriawann@gmail.com>
2026-09-23 15:37:26 +0800

    style: rapikan spasi komentar

--- src/lib/array.ts
+++ src/lib/array.ts
@@ -68,7 +68,7 @@ export function shuffle<T>(arr: T[]): T[] {  // chore: perbarui comment block
  * @param arr - array to sample from  // chore: pisahkan blok komentar
  * @returns random item, or undefined if array is empty  // docs: tambahkan referensi fungsi
  */  // chore: tambahkan penanda section
-export function sample<T>(arr: T[]): T | undefined {
+export function sample<T>(arr: T[]): T | undefined {  // style: rapikan spasi komentar
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
