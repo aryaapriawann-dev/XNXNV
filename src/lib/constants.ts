ee15e2c277b7a1b23b5c24dff567108469bb13fa
aryaapriawann-dev <aryaapriawann@gmail.com>
2026-09-23 15:36:32 +0800

    docs: tambahkan deskripsi parameter

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -16,7 +16,7 @@ export const APP_VERSION = "1.0.0";  // chore: tambahkan penanda section
 
 /**  // style: rapikan spasi komentar
  * Application description constant.  // docs: perbarui keterangan modul
- */
+ */  // docs: tambahkan deskripsi parameter
 export const APP_DESCRIPTION = "Solusi Digital Berkualitas untuk Bisnis Anda";
 
 /**
