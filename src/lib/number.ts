a28aaf2eb554051d945471bfed42cc563a871d6d
aryaapriawann-dev <aryaapriawann@gmail.com>
2026-09-23 15:35:26 +0800

    docs: perbarui keterangan fungsi

--- src/lib/number.ts
+++ src/lib/number.ts
@@ -45,7 +45,7 @@ export function formatPercent(value: number, decimals = 1): string {  // chore: 
 
 /**  // chore: tambahkan note implementasi
  * Clamp number between min and max  // style: rapikan penempatan comment
- *
+ *  // docs: perbarui keterangan fungsi
  * @param num - number to clamp
  * @param min - minimum value
  * @param max - maximum value
