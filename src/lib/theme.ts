39d4fb645f54d8ef958581417154a628c9cb2556
aryaapriawann-dev <aryaapriawann@gmail.com>
2026-09-23 15:28:35 +0800

    style: rapikan indentasi docstring

--- src/lib/theme.ts
+++ src/lib/theme.ts
@@ -3,6 +3,7 @@
 // style: rapikan indentasi docstring
 // style: rapikan indentasi docstring
 // style: rapikan indentasi docstring
+// style: rapikan indentasi docstring
 /**
  * Theme type for light, dark, or system preference.
  */
