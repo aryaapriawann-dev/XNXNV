6f6da162fd5f1f13eb545e0b57ae34356bde840b
aryaapriawann-dev <aryaapriawann@gmail.com>
2026-09-23 15:28:01 +0800

    docs: tambah komentar tipe pada fungsi

--- src/components/Skeleton.tsx
+++ src/components/Skeleton.tsx
@@ -1,6 +1,7 @@
 // docs: tambah komentar tipe pada fungsi
 // docs: tambah komentar tipe pada fungsi
 // docs: tambah komentar tipe pada fungsi
+// docs: tambah komentar tipe pada fungsi
 "use client";
 /**
  * Skeleton component for loading placeholders.
